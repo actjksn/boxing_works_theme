f4c103155305a198f9d2271b9e8cec6d5a521516
Jay Liu <jaycliu@gmail.com>
2014-03-13 00:49:19 -0700

    Add logic functionality in case of missing plugin.

--- functions.php
+++ functions.php
@@ -36,109 +36,108 @@ function count_widgets($sidebar_id) {
 	return $total;
 }
 
+if ( class_exists('Simple_Image_Widget') ) {
+	class Slide_Widget extends Simple_Image_Widget {
+		/**
+		 * Setup widget options.
+		 */
+		function __construct() {
+			$widget_options = wp_parse_args( $widget_options, array(
+					'classname' => 'widget_slide',
+					'description' => 'Add a slide to the homepage slider'
+					) );
 
-class Slide_Widget extends Simple_Image_Widget {
-	/**
-	 * Setup widget options.
-	 */
-	function __construct() {
-		$widget_options = wp_parse_args( $widget_options, array(
-				'classname' => 'widget_slide',
-				'description' => 'Add a slide to the homepage slider'
-				) );
-
-		parent::__construct( 'slide', 'Slide', $widget_options );
-	}
-
-	function widget( $args, $instance ) {
-		// Copy the original title so it can be passed to hooks.
-		$instance['title_raw'] = $instance['title'];
-		$instance['title'] = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
-
-		// Copy the original text so it can be passed to hooks.
-		$instance['text_raw'] = $instance['text'];
-		$instance['text'] = apply_filters( 'widget_text', empty( $instance['text'] ) ? '' : $instance['text'], $instance, $this->id_base );
-
-		// Start building the output.
-		$output = '';
-
-		// Make sure the image ID is a valid attachment.
-		if ( ! empty( $instance['image_id'] ) ) {
-			$image = get_post( $instance['image_id'] );
-			if ( ! $image || 'attachment' != get_post_type( $image ) ) {
-				$output = '<!-- Image Widget Error: Invalid Attachment ID -->';
-			}
+			parent::__construct( 'slide', 'Slide', $widget_options );
 		}
 
-		if ( empty( $output ) ) {
-			$output = $this->render( $args, $instance );
-		}
+		function widget( $args, $instance ) {
+			// Copy the original title so it can be passed to hooks.
+			$instance['title_raw'] = $instance['title'];
+			$instance['title'] = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
 
-		echo $output;
-	}	
+			// Copy the original text so it can be passed to hooks.
+			$instance['text_raw'] = $instance['text'];
+			$instance['text'] = apply_filters( 'widget_text', empty( $instance['text'] ) ? '' : $instance['text'], $instance, $this->id_base );
 
-	function render ( $args, $instance ) {
-		$instance['link_open'] = '';
-		$instance['link_close'] = '';
-		if ( ! empty ( $instance['link'] ) ) {
-			$target = ( empty( $instance['new_window'] ) ) ? '' : ' target="_blank"';
-			$instance['link_open'] = '<a href="' . esc_url( $instance['link'] ) . '"' . $target . '>';
-			$instance['link_close'] = '</a>';
-		}
+			// Start building the output.
+			$output = '';
 
-		$output = $args['before_widget'];
-
-			// Allow custom output to override the default HTML.
-			if ( $inside = apply_filters( 'simple_image_widget_output', '', $args, $instance, $this->id_base ) ) {
-				$output .= $inside;
-			} else {
-				$output .= ( empty( $instance['title'] ) ) ? '' : $args['before_title']. $instance['title'] . $args['after_title'];
-
-				// Add the image.
-				if ( ! empty( $instance['image_id'] ) ) {			
-					$image_size = ( ! empty( $instance['image_size'] ) ) ? $instance['image_size'] : apply_filters( 'simple_image_widget_output_default_size', 'medium', $this->id_base );
-					
-					$default_attr = array(
-						'class' => 'img-responsive',
-						'alt'   => trim(strip_tags( get_post_meta($attachment_id, '_wp_attachment_image_alt', true) )),
-					);
-					
-					$output .= sprintf( '%s%s%s',
-						$instance['link_open'],
-						wp_get_attachment_image( $instance['image_id'], $image_size, 0, $default_attr ),
-						$instance['link_close']
-					);
-				} elseif ( ! empty( $instance['image'] ) ) {
-					// Legacy output.
-					$output .= sprintf( '%s<img src="%s" alt="%s">%s',
-						$instance['link_open'],
-						esc_url( $instance['image'] ),
-						( empty( $instance['alt'] ) ) ? '' : esc_attr( $instance['alt'] ),
-						$instance['link_close']
-					);
-				}
-
-				// Add the text.
-				if ( ! empty( $instance['text'] ) ) {
-					$output .= '<div class="carousel-caption">' . apply_filters( 'the_content', $instance['text'] ) . '</div>';
-				}
-
-				// Add a more link.
-				if ( ! empty( $instance['link_open'] ) && ! empty( $instance['link_text'] ) ) {
-					$output .= '<p class="more">' . $instance['link_open'] . $instance['link_text'] . $instance['link_close'] . '</p>';
+			// Make sure the image ID is a valid attachment.
+			if ( ! empty( $instance['image_id'] ) ) {
+				$image = get_post( $instance['image_id'] );
+				if ( ! $image || 'attachment' != get_post_type( $image ) ) {
+					$output = '<!-- Image Widget Error: Invalid Attachment ID -->';
 				}
 			}
 
-		$output .= $args['after_widget'];
+			if ( empty( $output ) ) {
+				$output = $this->render( $args, $instance );
+			}
 
-		return $output;
-	}		
-}
+			echo $output;
+		}	
 
-function load_widgets_slider() {
-	if ( class_exists('Simple_Image_Widget') ) {
-		register_widget( 'Slide_Widget' );
+		function render ( $args, $instance ) {
+			$instance['link_open'] = '';
+			$instance['link_close'] = '';
+			if ( ! empty ( $instance['link'] ) ) {
+				$target = ( empty( $instance['new_window'] ) ) ? '' : ' target="_blank"';
+				$instance['link_open'] = '<a href="' . esc_url( $instance['link'] ) . '"' . $target . '>';
+				$instance['link_close'] = '</a>';
+			}
+
+			$output = $args['before_widget'];
+
+				// Allow custom output to override the default HTML.
+				if ( $inside = apply_filters( 'simple_image_widget_output', '', $args, $instance, $this->id_base ) ) {
+					$output .= $inside;
+				} else {
+					$output .= ( empty( $instance['title'] ) ) ? '' : $args['before_title']. $instance['title'] . $args['after_title'];
+
+					// Add the image.
+					if ( ! empty( $instance['image_id'] ) ) {			
+						$image_size = ( ! empty( $instance['image_size'] ) ) ? $instance['image_size'] : apply_filters( 'simple_image_widget_output_default_size', 'medium', $this->id_base );
+						
+						$default_attr = array(
+							'class' => 'img-responsive',
+							'alt'   => trim(strip_tags( get_post_meta($attachment_id, '_wp_attachment_image_alt', true) )),
+						);
+						
+						$output .= sprintf( '%s%s%s',
+							$instance['link_open'],
+							wp_get_attachment_image( $instance['image_id'], $image_size, 0, $default_attr ),
+							$instance['link_close']
+						);
+					} elseif ( ! empty( $instance['image'] ) ) {
+						// Legacy output.
+						$output .= sprintf( '%s<img src="%s" alt="%s">%s',
+							$instance['link_open'],
+							esc_url( $instance['image'] ),
+							( empty( $instance['alt'] ) ) ? '' : esc_attr( $instance['alt'] ),
+							$instance['link_close']
+						);
+					}
+
+					// Add the text.
+					if ( ! empty( $instance['text'] ) ) {
+						$output .= '<div class="carousel-caption">' . apply_filters( 'the_content', $instance['text'] ) . '</div>';
+					}
+
+					// Add a more link.
+					if ( ! empty( $instance['link_open'] ) && ! empty( $instance['link_text'] ) ) {
+						$output .= '<p class="more">' . $instance['link_open'] . $instance['link_text'] . $instance['link_close'] . '</p>';
+					}
+				}
+
+			$output .= $args['after_widget'];
+
+			return $output;
+		}		
 	}
-}
 
-add_action( 'widgets_init', 'load_widgets_slider' );
+	function load_widgets_slider() {
+			register_widget( 'Slide_Widget' );
+	}
+
+	add_action( 'widgets_init', 'load_widgets_slider' );
+}
--- sidebar-carousel-marketing.php
+++ sidebar-carousel-marketing.php
@@ -1,44 +1,76 @@
 <div id="carousel-marketing" class="carousel slide" data-ride="carousel" data-interval="10000">
   <ol class="carousel-indicators">
     <?php $total = count_widgets('carousel-marketing'); ?>
-    <?php for($i=0; $i < $total; $i++): ?>
-      <?php if($i == 0): ?>
-        <li data-target="#carousel-marketing" data-slide-to="<?php echo $i; ?>" class="active"></li>
-      <?php else: ?>
-        <li data-target="#carousel-marketing" data-slide-to="<?php echo $i; ?>"></li>
-      <?php endif; ?>
-    <?php endfor;?>
+    <?php include_once( ABSPATH . 'wp-admin/includes/plugin.php' ); ?>
+    <?php $slide_active = is_plugin_active('simple-image-widget/simple-image-widget.php') ?>
+
+    <?php if( ($total > 0) && $slide_active ): ?>
+      <?php for($i=0; $i < $total; $i++): ?>
+        <?php if($i == 0): ?>
+          <li data-target="#carousel-marketing" data-slide-to="<?php echo $i; ?>" class="active"></li>
+        <?php else: ?>
+          <li data-target="#carousel-marketing" data-slide-to="<?php echo $i; ?>"></li>
+        <?php endif; ?>
+      <?php endfor;?>   
+    <?php else: ?>
+      <li data-target="#carousel-marketing" data-slide-to="0" class="active"></li>
+      <li data-target="#carousel-marketing" data-slide-to="1"></li>
+      <li data-target="#carousel-marketing" data-slide-to="2"></li>       
+    <?php endif; ?>
   </ol>
 
   <div class="carousel-inner">
-    <?php
-      $sidebars = wp_get_sidebars_widgets();
-      $carousel = $sidebars['carousel-marketing'];
-      $pattern = '/\d+/';
-      $instance = get_option('widget_slide');
+    <?php if( ($total > 0) && $slide_active ): ?>
+      <?php
+        $sidebars = wp_get_sidebars_widgets();
+        $carousel = $sidebars['carousel-marketing'];
+        $pattern = '/\d+/';
+        $instance = get_option('widget_slide');
 
-      //var_dump($carousel);
-      //var_dump($instance);
-      $i = true;
-      foreach($carousel as $id) {
-        preg_match($pattern, $id, $matches);
-        $instance_id = $matches[0];
-        if ($i) {
-          $i = false;
-          $args = array (
-            'before_widget' => '<div class="item active">',
-            'after_widget'  => '</div>',
-          );
+        //var_dump($carousel);
+        //var_dump($instance);
+        $i = true;
+        foreach($carousel as $id) {
+          preg_match($pattern, $id, $matches);
+          $instance_id = $matches[0];
+          if ($i) {
+            $i = false;
+            $args = array (
+              'before_widget' => '<div class="item active">',
+              'after_widget'  => '</div>',
+            );
 
-          the_widget( 'Slide_Widget', $instance[$instance_id], $args);
-        } else {
-          $args = array (
-            'before_widget' => '<div class="item">',
-            'after_widget'  => '</div>',
-          );
-          the_widget( 'Slide_Widget', $instance[$instance_id], $args );
-        }
-      }      
-    ?>
+            the_widget( 'Slide_Widget', $instance[$instance_id], $args);
+          } else {
+            $args = array (
+              'before_widget' => '<div class="item">',
+              'after_widget'  => '</div>',
+            );
+            the_widget( 'Slide_Widget', $instance[$instance_id], $args );
+          }
+        }      
+      ?>
+    <?php else: ?>
+      <div class="item active">
+        <img src="http://placehold.it/1000x450" class="img-responsive">
+        <div class="carousel-caption">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis faucibus metus. Sed vel accumsan mauris. Mauris leo libero, pharetra id hendrerit quis, tempus vel arcu.
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="http://placehold.it/1000x450" class="img-responsive">
+        <div class="carousel-caption">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis faucibus metus. Sed vel accumsan mauris. Mauris leo libero, pharetra id hendrerit quis, tempus vel arcu.
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="http://placehold.it/1000x450" class="img-responsive">
+        <div class="carousel-caption">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis faucibus metus. Sed vel accumsan mauris. Mauris leo libero, pharetra id hendrerit quis, tempus vel arcu.
+        </div>
+      </div>       
+    <?php endif; ?>
   </div>
 </div>
